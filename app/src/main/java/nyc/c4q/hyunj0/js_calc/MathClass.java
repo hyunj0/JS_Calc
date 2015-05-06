d12b268b24d34f181ea786afe5aa05c34814afa0
hyunj0 <hyunj0@hotmail.com>
2015-05-05 21:16:46 -0400

    Created JS_Calc project

---
+++ app/src/main/java/nyc/c4q/hyunj0/js_calc/MathClass.java
@@ -0,0 +1,9 @@
+package nyc.c4q.hyunj0.js_calc;
+
+//Will contain all the math operations in this class
+
+public class MathClass {
+    /*  Basic calculator will have the basic math operations as well as the power, squareroot, and percentage function.
+        Advanced will contain all in basic calculator and everything else.
+     */
+}
